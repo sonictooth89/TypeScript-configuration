0b50eb7109c879fcec03186dc423231e7cea6e00
Malgorzata Nowak <nowak.em@interia.eu>
2023-01-30 19:56:20 +0100

    app.ts modified

--- app.ts
+++ app.ts
@@ -1,4 +1,21 @@
-window.onload = () => {
-    const button = document.getElementById('hello');
-    alert('It has readed!')
-}
+/**
+ *Celem zadania jest napisanie programu, który obliczy koszt zakupów w sklepie z telefonami komórkowymi. Mając daną kwotę, program będzie kupował telefony dopóki starcza mu pieniędzy. Dopóki nie przekroczona zostanie pewna kwota pośrednia, do każdego telefony program dobierze jeszcze jakiś dodatek. Kwoty podawane są bez podatków, więc program musi do tego obliczyć jeszcze wydatek brutto. Na koniec program powinien wypisać ile kasy zostało wydane, informacja ta ma być odpowiednio sformatowana (znak dolara z przodu i dokładność dziesiętna kwoty do dwóch miejsc po przecinku).
+ */
+
+const amount: number = 10000;
+
+const cellPhonePrice: number = 99.99;
+
+const gift: number = 6.45;
+
+const tax = 0.23;
+
+let spend: number = 0;
+
+while (spend < amount) {
+    spend = spend + cellPhonePrice * (1 + tax);
+};
+
+
+
+console.log(`Z kwoty ${amount} zostalo wydane ${spend} zl`)
